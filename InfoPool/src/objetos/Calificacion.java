46249d251a7b6b794d8b996e70f8f480d96e8a73
NELSON ARIEL VICTOR ZAPATA <nelsonarielvictorzapata08@gmail.com>
2014-11-07 01:33:42 -0300

    mas getters y setters
    g y s

--- InfoPool/src/objetos/Calificacion.java
+++ InfoPool/src/objetos/Calificacion.java
@@ -4,6 +4,27 @@ public class Calificacion {
 	private int id;
 	private boolean esPositiva;
 	private String comentario;
+	private Viajero calificado;
+	private Viajero autor;
+	private Viaje viaje;
+	public Viajero getCalificado() {
+		return calificado;
+	}
+	public void setCalificado(Viajero calificado) {
+		this.calificado = calificado;
+	}
+	public Viajero getAutor() {
+		return autor;
+	}
+	public void setAutor(Viajero autor) {
+		this.autor = autor;
+	}
+	public Viaje getViaje() {
+		return viaje;
+	}
+	public void setViaje(Viaje viaje) {
+		this.viaje = viaje;
+	}
 	private void  setId(int id){
 		this.id=id;
 	}
